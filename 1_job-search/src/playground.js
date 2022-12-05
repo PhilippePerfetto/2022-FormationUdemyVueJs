e5b1dd97156287215b08842691a81f0621bb52fd
Philippe <philippe.perfetto@hotmail.fr>
2022-12-05 17:28:30 +0100

    New component in new grid. Playground tests on delay/timer

--- 1_job-search/src/playground.js
+++ 1_job-search/src/playground.js
@@ -5,3 +5,15 @@ const goodFoods = {
 };
 
 console.log(goodFoods);
+
+const interval = setInterval(() => {
+  console.log("I will print each 2s after the prog starts");
+}, 2000);
+
+console.log(interval);
+
+setTimeout(() => {
+  clearInterval(interval);
+  console.log("Ending interval timer");
+  console.log(interval);
+}, 8000);
