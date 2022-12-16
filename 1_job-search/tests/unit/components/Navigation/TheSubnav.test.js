388c057d7206c78c369fe839e65b99eb1111bc5a
Philippe <philippe.perfetto@hotmail.fr>
2022-12-16 16:50:34 +0100

    Tests refactored, adding a new job result view

--- 1_job-search/tests/unit/components/Navigation/TheSubnav.test.js
+++ 1_job-search/tests/unit/components/Navigation/TheSubnav.test.js
@@ -3,20 +3,24 @@ import { render, screen } from "@testing-library/vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
 
 describe("TheSubnav", () => {
-  describe("when user is on jobs page", () => {
-    it("displays job count", () => {
-      render(TheSubnav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
+  const renderTheSubnav = (routeName) => {
+    render(TheSubnav, {
+      global: {
+        mocks: {
+          $route: {
+            name: routeName,
           },
         },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
+        stubs: {
+          FontAwesomeIcon: true,
         },
-      });
+      },
+    });
+  };
+
+  describe("when user is on jobs page", () => {
+    it("displays job count", () => {
+      renderTheSubnav("JobResults");
 
       const jobCount = screen.getByText("1653");
 
@@ -26,18 +30,7 @@ describe("TheSubnav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
-      render(TheSubnav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
-        },
-      });
+      renderTheSubnav("Home");
 
       const jobCount = screen.queryByText("1653");
 
