d46cf20a998301333101f983dd23b03b45060262
Philippe <philippe.perfetto@hotmail.fr>
2022-12-18 17:06:07 +0100

    Binding on JSON jobs file, adding tests for job listing components and a json db file with 100 jobs

--- 1_job-search/src/components/JobResults/JobListings.vue
+++ 1_job-search/src/components/JobResults/JobListings.vue
@@ -1,18 +1,32 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <job-listing />
-      <job-listing />
-      <job-listing />
+      <job-listing v-for="job in displayedJobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script>
+import axios from "axios";
+
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 export default {
   name: "JobListings",
   components: { JobListing },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  computed: {
+    displayedJobs() {
+      return this.jobs.slice(0, 10);
+    },
+  },
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/jobs");
+    this.jobs = response.data;
+  },
 };
 </script>
