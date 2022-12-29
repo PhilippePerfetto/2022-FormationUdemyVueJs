0b5c149eb7b5de93ae5a66409a5a8d4b0f335960
Philippe <philippe.perfetto@hotmail.fr>
2022-12-29 16:53:05 +0100

    starting to add filters

--- 1_job-search/src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ 1_job-search/src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,12 +2,48 @@
   <div
     class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do ?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filters" type="secondary" />
+        </div>
+      </div>
+
+      <collapsible-accordeon>
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-8 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="Between Vue" type="checkbox" class="mr-3" />
+                <label for="Between Vue and Me">Between Vue...</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="h-8 w-1/2">
+                <input id="Vue and a half men" type="checkbox" class="mr-3" />
+                <label for="Vue and a half men">Vue and a half...</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </collapsible-accordeon>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import CollapsibleAccordeon from "@/components/Shared/CollapsibleAccordeon.vue";
+
 export default {
   name: "JobFiltersSidebar",
+  components: { ActionButton, CollapsibleAccordeon },
 };
 </script>
